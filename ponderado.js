00b89e198d47b43c5198dddf979fe68795ee3b04
Viruzzz <nahuelsinka@gmail.com>
2022-04-23 18:10:36 -0300

    Calcula promedio ponderado dinamico

--- ponderado.js
+++ ponderado.js
@@ -1,9 +1,29 @@
+// const examenParcial = 2;
+// const examenFinal= 4;
+
+// function calculaPonderado(nota1, nota2, nota3, notaFinal) {
+//     const sumaPonderada = (nota1 * examenParcial) + (nota2 * examenParcial) + (nota3 * examenParcial) + (notaFinal * examenFinal);
+
+//     const result = sumaPonderada / 10;
+//     console.log(result);
+// }
+
 const examenParcial = 2;
-const examenFinal= 4;
+const examenFinal = 4;
 
-function calculaPonderado(nota1, nota2, nota3, notaFinal) {
-    const sumaPonderada = (nota1 * examenParcial) + (nota2 * examenParcial) + (nota3 * examenParcial) + (notaFinal * examenFinal);
+function calculaPonderado(notasParcial, notaFinal) {
+    // Se optiene la nota maxima posible segun la catidad de parciales
+    const canditadParcial = notasParcial.length;
+    const notaMax = (canditadParcial * examenParcial) + examenFinal;
 
-    const result = sumaPonderada / 10;
-    console.log(result);
+    // Sumamos el total de las notas parciales mas la nota final
+    const notasParcialOpacion1 = notasParcial.map(function (a) {
+        return a * examenParcial;
+    })
+
+    const sumaTotal = notasParcialOpacion1.reduce((ele1, ele2) => ele1 + ele2, 0) + (notaFinal * examenFinal);
+
+    // Division de la suma de las notas entre la nota maxima
+    const result = sumaTotal / notaMax;
+    return result
 }
